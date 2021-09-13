e060f0003c614738fc4d346164d7d14764c1e7bb
Reggie Lucernas <rlucernas@paybyphone.com>
2021-09-12 21:10:06 -0700

    added starter code

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	//"encoding/csv"
-	//"flag"
+	// "encoding/csv"
+	"flag"
 	"fmt"
-	//"os"
-	//"strings"
+	"os"
+	// "strings"
 )
 
 func main() {
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	flag.Parse()
 
-	fmt.Println("Hello world!")
+	fmt.Println("filename:", *csvFilename)
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		fmt.Printf("Failed to open the CSV file: %s\n", *csvFilename)
+		os.Exit(1)
+	}
+	fmt.Println(*file)
 
 }
